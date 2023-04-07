70288716731578e6fe2906619ba9049d4045d476
Anton Filippov <anton@filippov.im>
2023-04-07 15:36:36 +0700

    update test includes - is project top level?

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,3 +1,10 @@
 
 set(thirdparty_DIR "${CMAKE_SOURCE_DIR}/thirdparty/cmake" CACHE STRING "thiredparty dir" FORCE)
 set(conan_DIR "${CMAKE_SOURCE_DIR}/thirdparty/cmake" CACHE STRING "conan dir" FORCE)
+
+if(CMAKE_VERSION VERSION_LESS 3.21)
+  get_property(not_top DIRECTORY PROPERTY PARENT_DIRECTORY)
+  if(NOT not_top)
+    set(PROJECT_IS_TOP_LEVEL true)
+  endif()
+endif()
